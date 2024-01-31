197e499e44866aa1a64f62d9751560e50f9def37
Jon Boone <ipmonger@delamancha.org>
2024-01-31 13:39:50 -0500

    update the doc strings

--- python/currency-exchange/exchange.py
+++ python/currency-exchange/exchange.py
@@ -9,7 +9,7 @@ https://www.compareremit.com/money-transfer-tips/guide-to-exchanging-currency-fo
 
 
 def exchange_money(budget, exchange_rate):
-    """
+    """Calculate the value of the foreign currency received in the exchange.
 
     :param budget: float - amount of money you are planning to exchange.
     :param exchange_rate: float - unit value of the foreign currency.
@@ -20,7 +20,7 @@ def exchange_money(budget, exchange_rate):
 
 
 def get_change(budget, exchanging_value):
-    """
+    """Calculate the redisual budget value after the exchange.
 
     :param budget: float - amount of money you own.
     :param exchanging_value: float - amount of your money you want to exchange.
@@ -31,7 +31,7 @@ def get_change(budget, exchanging_value):
 
 
 def get_value_of_bills(denomination, number_of_bills):
-    """
+    """Calculate the total value of the exchanged currency.
 
     :param denomination: int - the value of a bill.
     :param number_of_bills: int - total number of bills.
@@ -42,7 +42,7 @@ def get_value_of_bills(denomination, number_of_bills):
 
 
 def get_number_of_bills(amount, denomination):
-    """
+    """Calculate the number of bills received in the exchange.
 
     :param amount: float - the total starting value.
     :param denomination: int - the value of a single bill.
@@ -53,7 +53,7 @@ def get_number_of_bills(amount, denomination):
 
 
 def get_leftover_of_bills(amount, denomination):
-    """
+    """Calculate the excess currency value kept by the exchange.
 
     :param amount: float - the total starting value.
     :param denomination: int - the value of a single bill.
@@ -64,7 +64,7 @@ def get_leftover_of_bills(amount, denomination):
 
 
 def exchangeable_value(budget, exchange_rate, spread, denomination):
-    """
+    """Calculate the maximum value of the currency received in the exchange.
 
     :param budget: float - the amount of your money you want to exchange.
     :param exchange_rate: float - the unit value of the foreign currency.
