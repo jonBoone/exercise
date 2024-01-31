f31922fc85474c09f7f8bded791f6ef5ad034cfd
Jon Boone <ipmonger@delamancha.org>
2024-01-30 20:23:47 -0500

    implement is_critically_balanced and correct PEP8 E501

--- python/meltdown-mitigation/conditionals.py
+++ python/meltdown-mitigation/conditionals.py
@@ -4,17 +4,20 @@
 def is_criticality_balanced(temperature, neutrons_emitted):
     """Verify criticality is balanced.
 
-    :param temperature: int or float - temperature value in kelvin.
-    :param neutrons_emitted: int or float - number of neutrons emitted per second.
+    :param temperature: int or float - temperature in kelvin.
+    :param neutrons_emitted: int or float - neutrons emitted per second.
     :return: bool - is criticality balanced?
 
     A reactor is said to be critical if it satisfies the following conditions:
     - The temperature is less than 800 K.
-    - The number of neutrons emitted per second is greater than 500.
-    - The product of temperature and neutrons emitted per second is less than 500000.
+    - The neutrons emitted per second is greater than 500.
+    - temperature * neutrons emitted per second less than 500000.
     """
+    critical_temperature = temperature < 800
+    critical_neutrons = neutrons_emitted > 500
+    critical_reaction = (temperature * neutrons_emitted) < 500000
 
-    pass
+    return critical_temperature and critical_neutrons and critical_reaction
 
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
