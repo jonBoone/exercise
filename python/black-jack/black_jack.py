82c13cc957b7bd754c43020c12136824aef99f80
Jon Boone <ipmonger@delamancha.org>
2024-01-30 23:43:50 -0500

    correcting long line errors

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -9,7 +9,7 @@ def value_of_card(card):
     """Determine the scoring value of a card.
 
     :param card: str - given card.
-    :return: int - value of a given card.  See below for values.
+    :return: int - value of a given card.  See values below.
 
     1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
     2.  'A' (ace card) = 1
@@ -22,8 +22,9 @@ def value_of_card(card):
 def higher_card(card_one, card_two):
     """Determine which card has a higher value in the hand.
 
-    :param card_one, card_two: str - cards dealt in hand.  See below for values.
-    :return: str or tuple - resulting Tuple contains both cards if they are of equal value.
+    :param card_one, card_two: str - cards dealt in hand.  See values below.
+    :return: str or tuple - resulting Tuple contains both cards if they have
+      equal value.
 
     1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
     2.  'A' (ace card) = 1
@@ -65,7 +66,8 @@ def can_split_pairs(card_one, card_two):
     """Determine if a player can split their hand into two hands.
 
     :param card_one, card_two: str - cards dealt.
-    :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
+    :return: bool - can the hand be split into two pairs? (i.e. are the cards
+      of the same value).
     """
 
     pass
@@ -75,7 +77,7 @@ def can_double_down(card_one, card_two):
     """Determine if a blackjack player can place a double down bet.
 
     :param card_one, card_two: str - first and second cards in hand.
-    :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
+    :return: bool - can the hand be doubled down? (i.e. sum of 9, 10 or 11).
     """
 
     pass
