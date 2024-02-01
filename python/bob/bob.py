c564a3ff251a439421486b574140af2eb9dd133e
Jon Boone <ipmonger@delamancha.org>
2024-02-01 13:49:02 -0500

    add module docstring and default response()

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,11 @@
+"""Determine reponses Bob will have to given inputs."""
+
+
 def response(hey_bob):
-    pass
+    """Return Bob's response to the input.
+
+    :param hey_bob: str - the input to which Bob must respond.
+    :return: str - Bob's response
+    """
+
+    return "Whatever."
