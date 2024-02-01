23e27a6ce35f88538a1c7567aec5922f3d15ca34
Jon Boone <ipmonger@delamancha.org>
2024-02-01 03:10:59 -0500

    implementation of leap_year()

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,24 @@
+"""Validate leap years"""
+
+
 def leap_year(year):
-    pass
+    """Validate a given year is a leap year
+
+    :param year: int - a specific year in the Gregorian calendar
+    :return: bool - return True if year is a leap year, False otherwise
+    """
+
+    divisible_by_four = year % 4 == 0
+    divisible_by_hundred = year % 100 == 0
+    divisible_by_four_hundred = year % 400 == 0
+
+    if divisible_by_four_hundred:
+        return True
+
+    if divisible_by_hundred:
+        return False
+
+    if divisible_by_four:
+        return True
+
+    return False
