316d93e3b77edb7d124a5f9de57d106b6ca9e708
Jon Boone <ipmonger@delamancha.org>
2024-02-01 13:51:40 -0500

    implement and call is_question()

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,11 +1,27 @@
 """Determine reponses Bob will have to given inputs."""
 
 
+def is_question(input):
+    """Determine if input is a question.
+
+    :param input: str - the input to analyze.
+    :return: bool - True if input is a question, False otherwise.
+    """
+
+    if input[-1] == "?":
+        return True
+
+    return False
+
+
 def response(hey_bob):
     """Return Bob's response to the input.
 
     :param hey_bob: str - the input to which Bob must respond.
     :return: str - Bob's response
     """
+
+    if is_question(hey_bob):
+        return "Sure."
 
     return "Whatever."
