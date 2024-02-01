d8b0dc34f99f923add292c14ae3e2ac82a88cec9
Jon Boone <ipmonger@delamancha.org>
2024-02-01 13:31:49 -0500

    implementation of is_armstrong_number()

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -7,4 +7,15 @@ def is_armstrong_number(number):
     :param number: int - the number to validate
     :returns: bool - True when characteristics are present, False otherwise
     """
-    pass
+
+    number_string = str(number)
+    number_of_digits = len(number_string)
+    armstrong_sum = 0
+
+    for digit in number_string:
+        armstrong_sum += int(digit) ** number_of_digits
+
+    if armstrong_sum == number:
+        return True
+
+    return False
