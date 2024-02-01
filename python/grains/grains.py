c23936770a36fc2b9827098724e688f7fdd9f66c
Jon Boone <ipmonger@delamancha.org>
2024-02-01 03:43:39 -0500

    replaced exponentiation (**) with bitshifting (1 <<)

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -13,7 +13,7 @@ def square(number):
     if not 1 <= number <= 64:
         raise ValueError("square must be between 1 and 64")
 
-    return 2**(number - 1)
+    return 1 << (number - 1)
 
 
 def total():
@@ -22,4 +22,4 @@ def total():
     :return: the total number of grains on a chessboard (64!)
     """
 
-    return 2**64 - 1
+    return (1 << 64) - 1
