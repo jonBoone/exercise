86d00143381fd966ab30bd106247bc72b40604c0
Jon Boone <ipmonger@delamancha.org>
2024-02-01 01:15:17 -0500

    implement add_me_to_the_queue()

--- python/chaitanas-colossal-coaster/list_methods.py
+++ python/chaitanas-colossal-coaster/list_methods.py
@@ -11,7 +11,10 @@ def add_me_to_the_queue(express_queue, normal_queue, ticket_type, person_name):
     :return: list - the (updated) queue the name was added to.
     """
 
-    pass
+    if ticket_type:
+        return express_queue.append(person_name)
+
+    return normal_queue.append(person_name)
 
 
 def find_my_friend(queue, friend_name):
