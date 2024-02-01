7caae919f65212527c786680cd91e1b6006ef5af
Jon Boone <ipmonger@delamancha.org>
2024-02-01 13:21:15 -0500

    added is_armstrong_number() docstring

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -2,4 +2,9 @@
 
 
 def is_armstrong_number(number):
+    """Validate number has the Armstrong characteristics.
+
+    :param number: int - the number to validate
+    :returns: bool - True when characteristics are present, False otherwise
+    """
     pass
