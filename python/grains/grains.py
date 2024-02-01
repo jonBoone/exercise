8d24c02339015f1482d15c1d19b6953557b9b989
Jon Boone <ipmonger@delamancha.org>
2024-02-01 03:29:45 -0500

    implementation of factorial() and factorial_helper()

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,33 @@
 """Calculate the number of grains a chessboard holds."""
 
 
+def factorial_helper(number, result):
+    """Calculate the factorial of a given number.
+
+    :param number: int - the particular factorial to compute
+    :param result: int - an accumulator of the running total
+    :return: int - the result when number == 0
+    """
+
+    if result == 0:
+        result = 1
+
+    if number == 0:
+        return result
+
+    return factorial_helper(number - 1, result * number)
+
+
+def factorial(number):
+    """Calculate factorial using standard math API.
+
+    :param number: int - the factorial to calculate
+    :return: int - the factorial of number
+    """
+
+    return factorial_helper(number, 0)
+
+
 def square(number):
     """Calculate the number of grains on a specified square.
 
