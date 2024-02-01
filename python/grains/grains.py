15b251eff1e6e021647762a8dcfe2f0b59b424e6
Jon Boone <ipmonger@delamancha.org>
2024-02-01 03:40:20 -0500

    remove tail-recursive factorial implementation and implement total()

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,33 +1,6 @@
 """Calculate the number of grains a chessboard holds."""
 
 
-def factorial_helper(number, result):
-    """Calculate the factorial of a given number.
-
-    :param number: int - the particular factorial to compute
-    :param result: int - an accumulator of the running total
-    :return: int - the result when number == 0
-    """
-
-    if result == 0:
-        result = 1
-
-    if number == 0:
-        return result
-
-    return factorial_helper(number - 1, result * number)
-
-
-def factorial(number):
-    """Calculate factorial using standard math API.
-
-    :param number: int - the factorial to calculate
-    :return: int - the factorial of number
-    """
-
-    return factorial_helper(number, 0)
-
-
 def square(number):
     """Calculate the number of grains on a specified square.
 
@@ -44,4 +17,9 @@ def square(number):
 
 
 def total():
-    pass
+    """Calculate the toal number of grains on a chessboard.
+
+    :return: the total number of grains on a chessboard (64!)
+    """
+
+    return 2**64 - 1
