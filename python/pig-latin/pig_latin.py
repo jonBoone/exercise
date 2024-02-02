230e5fe5a6e03b24dac171bef9e2a8454476d95c
Jon Boone <ipmonger@delamancha.org>
2024-02-02 07:22:44 -0500

    test for beginning_vowel_sounds()

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,14 +1,42 @@
 """Provide Pig Latin functionality."""
 
 
-def beginning_vowel(text):
+vowel_sounds = ('xr', 'yt')
+vowels = ('a','e','i','o','u')
+
+def begins_with_vowel_sound(word):
+    """Validate the given English word begins with a vowel sound.
+
+    :param word: str - the English word to check
+    :result: str - True, when word begins with a vowel cluster, False otherwise
+    """
+
+    if word[:2] in vowel_sounds:
+        return True
+
+    beginning_vowel_cluster = ''
+
+    for component in word:
+        if component in vowels:
+            beginning_vowel_cluster += component
+        else:
+            break
+
+    if '' != beginning_vowel_cluster:
+        return True
+
+    return False
+
+
+def add_ay_to_end(word):
     """Add 'ay' sound to the end of the word.
 
     :param text: str - word beginning with a vowel sound.
     :return: str - the word with an 'ay' suffix added
     """
 
-    return text + "ay"
+    return word + 'ay'
+
 
 def translate(text):
     """Translate a provided English text into Pig Latin.
