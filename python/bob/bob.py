58fb556c82e8d3e2f770b8f1d9e296bd3fca6890
Jon Boone <ipmonger@delamancha.org>
2024-02-02 06:17:15 -0500

    fixing linting problems

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,40 +1,40 @@
 """Determine reponses Bob will have to given inputs."""
 
 
-def is_question(input):
+def is_question(message):
     """Determine if input is a question.
 
     :param input: str - the input to analyze.
     :return: bool - True if input is a question, False otherwise.
     """
 
-    if input[-1] == "?":
+    if message[-1] == "?":
         return True
 
     return False
 
 
-def is_silence(input):
+def is_silence(message):
     """Determine if the input is silence.
 
     :param input: str - the input to analyze.
     :return: bool - True if empty string, False otherwise.
     """
 
-    if input == '':
+    if message == '':
         return True
 
     return False
 
 
-def is_yelling(input):
+def is_yelling(message):
     """Determine if the input is being yelled.
 
     :param input: str - the input to analyze.
     :return: bool - True if input is in ALL_CAPS, False otherwise.
     """
 
-    for letter in input.split():
+    for letter in message.split():
         if not letter.isupper():
             return False
 
@@ -52,8 +52,8 @@ def response(hey_bob):
     hey_bob = hey_bob.strip()
     if is_silence(hey_bob):
         return "Fine. Be that way!"
-    
-     # determine if the input is a question and/or is being yelled
+
+    # determine if the input is a question and/or is being yelled
     question = is_question(hey_bob)
     yelled = is_yelling(hey_bob)
 
@@ -65,6 +65,5 @@ def response(hey_bob):
 
     if yelled:
         return "Whoa, chill out!"
-
 
     return "Whatever."
