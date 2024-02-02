04926929bd634c3f9eabbfb698f0ee9e1af20233
Jon Boone <ipmonger@delamancha.org>
2024-02-02 07:07:08 -0500

    add function to handle beginning vowel sounds

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,5 +1,19 @@
 """Provide Pig Latin functionality."""
 
 
+def beginning_vowel(text):
+    """Add 'ay' sound to the end of the word.
+
+    :param text: str - word beginning with a vowel sound.
+    :return: str - the word with an 'ay' suffix added
+    """
+
+    return text + "ay"
+
 def translate(text):
+    """Translate a provided English text into Pig Latin.
+
+    :param text: str - the source English phrase.
+    :return: str - the translated Pig Latin phrase.
+    """
     pass
