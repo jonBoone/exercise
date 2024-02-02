135afec4f796e47d739cd5f5bdef37b8a12f6471
Jon Boone <ipmonger@delamancha.org>
2024-02-02 16:48:58 -0500

    initial implementation

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,5 +1,32 @@
-"""Convert integers into raindrop sounds (increased complexity version of FizzBuzz)"""
+"""Increased Complexity Version of FizzBuzz)"""
 
 
 def convert(number):
-    pass
+    """Convert intenger to corresponding raindrop sound.
+
+    :param number: int - the integer to convert
+    :return: str - the raindrop sounds corresponding to the number
+    """
+    divisible_by_three = number % 3 == 0
+    divisible_by_five = number % 5 == 0
+    divisible_by_seven = number % 7 == 0
+    found_divisor = False
+
+    raindrop_sounds = ''
+
+    if divisible_by_three:
+        raindrop_sounds += 'Pling'
+        found_divisor = True
+
+    if divisible_by_five:
+        raindrop_sounds += 'Plang'
+        found_divisor = True
+
+    if divisible_by_seven:
+        raindrop_sounds += 'Plong'
+        found_divisor = True
+
+    if not found_divisor:
+        raindrop_sounds = str(number)
+
+    return raindrop_sounds
