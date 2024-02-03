e667bbd0e65e88dff5f39d8b2e025e4c3e2c831b
Jon Boone <ipmonger@delamancha.org>
2024-02-03 17:24:27 -0500

    implementation of is_isogram()

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,23 @@
-def is_isogram(string):
-    pass
+"""Functionality to validate apparent isograms
+"""
+
+
+def is_isogram(string: str) -> bool:
+    """validate if the phrase is an isogram
+
+    :param string: the phrase to validate
+    :type string: str
+    :return: True if isogram, False otherwise
+    :rtype: bool
+    """
+    components: dict = {}
+
+    for component in string:  # type: str
+        component = component.lower() # type: str
+        if component in components.keys():  # type: bool
+            if component.isalpha():  # type: bool
+                return False  #rtype: bool
+
+        components[component] = True # type: bool
+
+    return True  # rtype: bool
