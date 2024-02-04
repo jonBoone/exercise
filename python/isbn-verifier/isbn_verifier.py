61e17e4ec7da8bdc1c529387852e30880e3df851
Jon Boone <ipmonger@delamancha.org>
2024-02-04 15:21:54 -0500

    initial implementation

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,30 @@
-def is_valid(isbn):
-    pass
+"""Validate ISBN-10 Numbers"""
+def is_valid(isbn: str) -> bool:
+    """Verify the given string represents a valid ISBN-10 number.
+
+    :param isbn: a presumed ISBN-10 number
+    :type isbn: str
+    :return: True when isbn validates properly, False otherwise
+    :rtype: bool
+    """
+
+    isbn_sans_formatting:str = isbn.replace('-', '')
+
+    if len(isbn_sans_formatting) != 10:
+        return False
+
+    sum = 0
+
+    for i in range(10, 0, -1):
+        digit_value = isbn_sans_formatting[-i]
+        if digit_value.isalpha():
+            if digit_value == 'X' and i == 1:
+                digit_value = '10'
+            else:
+                return False
+        sum += int(digit_value) * i
+
+    if sum % 11 != 0:
+        return False
+
+    return True
