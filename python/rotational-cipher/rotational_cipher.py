a5d06b9b395236334a614465ff4095c220b1660b
Jon Boone <ipmonger@delamancha.org>
2024-02-04 12:21:06 -0500

    relocate base latin alphabet and process each element of the text one at a time

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,5 +1,9 @@
 
 
+latin_alphabet: list[str] = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                                  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                                  's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+
 
 def generate_cipher(key: int) -> dict:
     """Generate a rotatinoal cipher using base lowecase latin alphabet.
@@ -9,13 +13,42 @@ def generate_cipher(key: int) -> dict:
     :return: the cipher alphabet
     :rtype: dict
     """
-    latin_alphabet: list[str] = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-                                  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                                  's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
     
     return {value: (index + key) % len(latin_alphabet) for index, value in enumerate(latin_alphabet)}
 
 
+
+def process_letter(letter: str, cipher: dict) -> str:
+    """Perform the appropriate substitution for the element provided.
+
+    :param letter: element of the text to substitute
+    :type letter: str
+    :param cipher: replacement alphabet
+    :type cipher: dict
+    :return: the substitution performed for the given element
+    :rtype: str
+    """
+    upcaseResult: bool = False
+    result: str = ''
+
+    if letter.isalpha():
+        if letter.isupper():
+            upcaseResult = True
+            letter = letter.lower()
+        result_index = cipher[letter]
+        result = latin_alphabet[result_index]
+        if upcaseResult:
+            result = result.upper()
+        return result
+
+    return letter
+
+
 def rotate(text: str, key: int) -> str:
     cipher = generate_cipher(key)
-    print(f'cipher == {cipher}')
+    result_text: list[str] = ''
+
+    for letter in text:
+        result_text = result_text + process_letter(letter, cipher)
+
+    return result_text
