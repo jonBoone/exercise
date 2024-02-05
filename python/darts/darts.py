ef79c79c738c11ac035651e69d830c998b418e2a
Jon Boone <ipmonger@delamancha.org>
2024-02-05 17:37:17 -0500

    adding constant values for calculation purposes

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,5 +1,21 @@
 """Calculate Darts Score"""
 
+# The Bullseye is the origin of the coordinate grid
+BULLSEYE: tuple(int, int) = (0, 0)
+
+# Coordinates with radious <= these enumerated values counts as being
+# within the corresponding circle.  Coordiantes with radius > 10 units
+# miss the target completely.
+
+INNERCIRCLE: int = 1      
+MIDDLECIRCLE: int = 5
+OUTERCIRCLE: int = 10
+
+# Scoring System
+INNERCIRCLESCORE: int = 10
+MIDDLECIRCLESCORE: int = 5
+OUTERCIRCLESCORE: int = 1
+MISSEDTARGETCOMPLETELY: int = 0
 
 def score(x: int, y: int) -> int:
     """Calculate score for a dart located at the point defined by x,y
