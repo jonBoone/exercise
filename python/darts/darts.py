75f3b785fcee626bc20d096e6386fe35ba1b7441
Jon Boone <ipmonger@delamancha.org>
2024-02-05 17:29:39 -0500

    added module and score() docstrings

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,14 @@
-def score(x, y):
+"""Calculate Darts Score"""
+
+
+def score(x: int, y: int) -> int:
+    """Calculate score for a dart located at the point defined by x,y
+
+    :param x: the location horizontally relative to the bullseye
+    :type x: int
+    :param y: the location vertically relative to the bullsyeye
+    :type y: int
+    :return: the number of points for landing at the coordinates
+    :rtype: int
+    """
     pass
