cfc7274dab0979b36bd050f6c4c9b839c2b9cce4
Jon Boone <ipmonger@delamancha.org>
2024-02-05 21:28:22 -0500

    add calls to and implementation of aliquot_sum()

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,6 +1,16 @@
 """Classify numbers according to it's aliquot sum."""
 
 
+def aliquot_sum(number: int) -> int:
+    """Calculate the aliquot sum of the given number.
+
+    :param number: the number of which to calculate the sum of it's factors
+    :type number: int
+    :return: the sum of all factors of the number excluding itself
+    :rtype: int
+    """
+    pass
+
 def classify(number: int) -> str:
     """ A perfect number equals the sum of its positive divisors.
 
@@ -9,4 +19,12 @@ def classify(number: int) -> str:
     :return: the classification of the input integer
     :rtype: str
     """
-    pass
+
+    aliquot: int = aliquot_sum(number)
+    if aliquot < number:
+        return 'deficient'
+
+    if aliquot > number:
+        return 'abundant'
+
+    return 'perfect'
