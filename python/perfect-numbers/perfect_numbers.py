0fae310e6443ff4efd5d00c8940d7825d1d3bcf9
Jon Boone <ipmonger@delamancha.org>
2024-02-05 21:20:55 -0500

    added module docstring and updated classify() docstring

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,7 +1,12 @@
-def classify(number):
+"""Classify numbers according to it's aliquot sum."""
+
+
+def classify(number: int) -> str:
     """ A perfect number equals the sum of its positive divisors.
 
-    :param number: int a positive integer
-    :return: str the classification of the input integer
+    :param number: the number to classify
+    :type number: int
+    :return: the classification of the input integer
+    :rtype: str
     """
     pass
